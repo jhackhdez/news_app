351371e4b0deb3226d7e7bc5ee6ec0d56f813720
Jorge Hernandez <jhackhdez@gmail.com>
2024-02-08 21:41:03 -0600

    news list design

--- lib/src/widgets/list_news.dart
+++ lib/src/widgets/list_news.dart
@@ -32,21 +32,77 @@ class _News extends StatelessWidget {
       children: [
         _TarjetaTopBar(news, index),
         _TarjetaTitulo(news),
-        _TarjetaImagen(news)
+        _TarjetaImagen(news),
+        _TarjetaBody(news),
+        _TarjetaBotones(),
+        const SizedBox(height: 10),
+        const Divider(),
       ],
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // RawMaterialButton otra clase de botones
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.red,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.star_border),
+        ),
+        const SizedBox(width: 10),
+        // RawMaterialButton otra clase de botones
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.more),
+        )
+      ],
+    );
+  }
+}
+
+class _TarjetaBody extends StatelessWidget {
+  final Article news;
+  const _TarjetaBody(this.news);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text((news.description != null) ? '${news.description}' : ''),
     );
   }
 }
 
 class _TarjetaImagen extends StatelessWidget {
   final Article news;
-
   const _TarjetaImagen(this.news);
 
   @override
   Widget build(BuildContext context) {
+    // ClipRRect crea el borde redondeado en la imagen
     return Container(
-      child: const Text('Hola Mundo'),
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+        child: Container(
+            // Condición ternaria para mostrar imagen de 'no-image' si en la noticia no viene una imagen de cabecera
+            child: (news.urlToImage != null)
+                ? FadeInImage(
+                    placeholder: const AssetImage('assets/img/giphy.gif'),
+                    image: NetworkImage('${news.urlToImage}'))
+                : const Image(image: AssetImage('assets/img/no-image.png'))),
+      ),
     );
   }
 }
@@ -83,7 +139,7 @@ class _TarjetaTopBar extends StatelessWidget {
           children: [
             Text(
               '${index + 1}. ',
-              style: TextStyle(color: myTheme.highlightColor),
+              style: TextStyle(color: myTheme.hintColor),
             ),
             Text('${news.source.name}. ')
           ],
